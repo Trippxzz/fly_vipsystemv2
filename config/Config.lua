4e41af821fe579e3879e8cbca4868311d2f9d814
yoeltripp <cris.ct2003@gmail.com>
2023-07-29 19:44:04 -0400

    Create Config.lua

---
+++ config/Config.lua
@@ -0,0 +1,53 @@
+Config = {}
+
+Config.Vips = {
+     {
+        Name = "Bronze",
+        Emoji = "🟫",
+        Ped = 'notavailable',
+        Cars = 1,
+        Money = 1000
+    },
+     {
+        Name = "Silver",
+        Emoji = "⬜",
+        Ped = 'notavailable',
+        Cars = 2,
+        Money = 2000
+    },
+    {
+        Name = "Gold",
+        Emoji = "🟨",
+        Ped = 1,
+        Cars = 3,
+        Money = 3000
+    },
+    {
+        Name = "Diamond",
+        Emoji = "🟦",
+        Ped = 1,
+        Cars = 5,
+        Money = 4000
+    }
+}
+
+
+Config.Cars = {
+     {model = "t20", label = "T20" ,category = "Bronze", imagen = "https://cdn.discordapp.com/attachments/1088923891503743117/1130602793128968342/T20.png"},
+     {model = "komoda", label = "Komoda", category = "Bronze", imagen = "https://cdn.discordapp.com/attachments/1088923891503743117/1130602889010761869/komoda.png"},
+     {model ="zentorno", label = "Zentorno", category ="Bronze", imagen = "https://cdn.discordapp.com/attachments/1088923891503743117/1130602998318506064/latest.png"},
+     {model = "adder", label = "Adder", category ="Bronze", imagen = "https://cdn.discordapp.com/attachments/1088923891503743117/1130603092673573025/latest.png"},
+     {model = "krieger", label = "Krieger", category = "Silver", imagen = "https://cdn.discordapp.com/attachments/1088923891503743117/1130603286274257067/latest.png"},
+     {model = "xa21", label = "Xa-21", category = "Silver", imagen = "https://cdn.discordapp.com/attachments/1088923891503743117/1130603367719256164/latest.png"},
+     {model = "toros", label = "Toros", category ="Silver", imagen = "https://cdn.discordapp.com/attachments/1088923891503743117/1130603456734973982/latest.png"},
+     {model = "vacca", label = "Vaca", category ="Silver", imagen = "https://cdn.discordapp.com/attachments/1088923891503743117/1130603541304705085/latest.png"},
+     {model = "t20", label = "T20" ,category = "Gold", imagen = "https://cdn.discordapp.com/attachments/1088923891503743117/1130602793128968342/T20.png"},
+     {model = "komoda", label = "Komoda", category = "Gold", imagen = "https://cdn.discordapp.com/attachments/1088923891503743117/1130602889010761869/komoda.png"},
+     {model ="zentorno", label = "Zentorno", category ="Gold", imagen = "https://cdn.discordapp.com/attachments/1088923891503743117/1130602998318506064/latest.png"},
+     {model = "adder", label = "Adder", category ="Gold", imagen = "https://cdn.discordapp.com/attachments/1088923891503743117/1130603092673573025/latest.png"},
+     {model = "krieger", label = "Krieger", category = "Diamond", imagen = "https://cdn.discordapp.com/attachments/1088923891503743117/1130603286274257067/latest.png"},
+     {model = "xa21", label = "Xa-21", category = "Diamond", imagen = "https://cdn.discordapp.com/attachments/1088923891503743117/1130603367719256164/latest.png"},
+     {model = "toros", label = "Toros", category ="Diamond", imagen = "https://cdn.discordapp.com/attachments/1088923891503743117/1130603456734973982/latest.png"},
+     {model = "vacca", label = "Vaca", category ="Diamond", imagen = "https://cdn.discordapp.com/attachments/1088923891503743117/1130603541304705085/latest.png"}
+    
+}
